f74044bf1fa245799e005f7eac0da896db4bb3bc
ValCaite <caitevalerie@gmail.com>
2023-06-16 09:45:06 -0700

    pint alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ *  main - Entry point
+ *
+ *  Return: Always 0
+ */
+
+int main(void)
+
+{
+
+	int lowerCase = 'a';
+
+	while (lowerCase <= 'z')
+	{
+		putchar(lowerCase);
+		lowerCase += 1;
+	}
+	putchr('\n');
+	return (0);
+}
