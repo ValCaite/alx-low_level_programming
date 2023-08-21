d179ee6761a090ec0c0343ba1405a7c2aa63d588
ValCaite <caitevalerie@gmail.com>
2023-08-21 04:33:31 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-#include <stddef.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <stddef.h>
 #include "main.h"
 
 /**
@@ -11,21 +15,28 @@
  * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
-{	int fd, w, len = 0;
+{
+	int fd;
+	int nletters;
+	int rwr;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
 
-	if (text_content != NULL)
-	{
-		for (len = 0; text_content[len];)
-			len++;
-	}
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-	fd = popen(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(fd, text_content, len);
+	if (fd == -1)
+		return (-1);
 
-	if (fd == -1 || w == -1)
+	if (!text_content)
+		text_content = "";
+
+	for (nletters = 0; text_content[nletters]; nletters++)
+		;
+
+	rwr = write(fd, text_content, nletters);
+
+	if (rwr == -1)
 		return (-1);
 
 	close(fd);
